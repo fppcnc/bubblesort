b47f15ba742aa980e753b7ef69b21ee21cd0f68d
Fil <concatofilippo94@gmail.com>
2023-06-29 12:02:36 +0200

    1st commit

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import {bubbleSort} from "./bubblesort";
 
 const mixedNumbers = [5, 4, 1, 2, 3];
+console.log(mixedNumbers);
 const sortedNumbers  = bubbleSort(mixedNumbers);
 console.log(sortedNumbers);
